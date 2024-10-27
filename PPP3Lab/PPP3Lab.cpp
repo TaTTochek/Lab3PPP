ca6191319916804a222ae9217e627a20a70b8e13
сергей фибих <fibikh1980@gmail.com>
2024-10-27 23:48:54 +0300

    Learning some code

--- PPP3Lab/PPP3Lab.cpp
+++ PPP3Lab/PPP3Lab.cpp
@@ -1,31 +1,76 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 
-class Tarif {
+using namespace std;
+
+class ITariff {
 protected:
     float cost;
-    std::string TName;
+    string TName;
 public:
-    Tarif(float cost, std::string TName) : cost(cost), TName(TName) { std::cout << "XyeCoC" << std::endl; }
-    ~Tarif() {}
+    ITariff(float cost, string TName) : cost(cost), TName(TName) { cout << "XyeCoC" << endl; }
+    virtual ~ITariff() { cout << "класс был удалён" << endl; }
 
-    virtual float GetCost() { return cost; }
-    virtual std::string GetName() { return TName; }
+    virtual float GetCost() const = 0;
+    virtual string GetName() { return TName; }
 };
-class PrekollTarif : public Tarif {
-public:
+
+class PrekollTarif : public ITariff {
+private:
     int XyeCoc;
 public:
-    PrekollTarif(float cost, std::string TName, int XyeCoc) : Tarif(cost, TName), XyeCoc(XyeCoc) {}
-    float GetCost() override { return (cost - XyeCoc); };
-    //PrekollTarif(float cost, std::string TName) : Tarif(cost, TName){}
+    PrekollTarif(float cost, string TName, int XyeCoc) : ITariff(cost, TName), XyeCoc(XyeCoc) {}
+    float GetCost() const override { return (cost - XyeCoc); };
+    //PrekollTarif(float cost, string TName) : Tarif(cost, TName){}
+    //~PrekollTarif() override { cout << "Сука" << endl; }
 };
+
+class ClassicTariff : public ITariff {
+public:
+    ClassicTariff(float cost, string TName) : ITariff(cost, TName) {};
+    float GetCost() const override { return cost; }
+};
+
+class Airport {
+private:
+    vector<ITariff*> tariffs;
+public:
+    ~Airport() {
+        for (ITariff* tarif : tariffs) delete tarif;
+        cout << "Airport was deleted" << endl;
+    }
+
+    void addTarif(ITariff* tarif) {
+        tariffs.push_back(tarif);
+    }
+    void ShowTariff() {
+        for (const auto& tarif : tariffs) {
+            cout << tarif->GetCost() << " " << tarif->GetName() << endl;
+        }
+    }
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    Tarif T(12, "123");
-    PrekollTarif Z(123.0123, "asd", 123);
-    std::cout << Z.GetCost();
+    system("chcp 1251");
+    //cout << "Hello World!\n";
+    //ITarif T(12, "123"); - Абстрактный класс
+    PrekollTarif Z(float(123.0123), "asd", 123);
+    ClassicTariff X(float(123.55), "Faggot");
+    cout << Z.GetCost() << endl;
+    cout << Z.GetName() << endl;
+    cout << X.GetCost() << endl;
+    cout << X.GetName() << endl;
+    Airport Y;
+    Y.addTarif(new ClassicTariff(123.22, "s"));
+    //Y.addTarif(&Z); - так не делать (проблема с удалением памяти)
+    Y.ShowTariff();
+    //cout << sizeof(Z) << " " << sizeof(X) << endl;
+    //Y.addTarif(Z)
+
+    return 0;
+
 
 }
